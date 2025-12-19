1b60b3b97a4333a5a09e59676124e3222aba1d01
siditech <69406327+sidi96056@users.noreply.github.com>
2025-12-19 07:44:55 +0000

    Update views.py

--- Dersyonetimsystemi/Dersyonetimsystemi/views.py
+++ Dersyonetimsystemi/Dersyonetimsystemi/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+# Create your views here.
+from django.http import HttpResponse
 
 
 
 def home(request):
     return render(request,"base.html")
 
+
 def login(request):
     return render(request,"login.html")
 
 def register(request):
-    return redirect(request,"register.html")
+    return render(request,"register.html")
+
+def subscribe(request):
+    return render(request,"subscribe.html")
+
+def paid(request):
+    return render(request,"paid.html")
